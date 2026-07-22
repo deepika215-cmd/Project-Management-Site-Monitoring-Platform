f03515217489da27abbfe88cf3d5848d26a5736c
deepika Seelam <sroinivasdeepika215@gmail.com>
2026-07-22 22:20:56 +0530

    Initial project setup

---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+from app.database.database import Base, engine
+from app.models.user import User
+from app.api.auth import router as auth_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="BuildTrack API")
+
+app.include_router(auth_router)
+
+
+@app.get("/")
+def home():
+    return {"message": "BuildTrack Backend Running Successfully"}
